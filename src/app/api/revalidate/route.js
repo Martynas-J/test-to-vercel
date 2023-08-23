793138500d9256994c0c002e28fe3d01ddad08dd
Martynas-J <Aurimhas@gmail.com>
2023-08-23 22:30:43 +0300

    add vercel v3.3

--- src/app/api/revalidate/route.js
+++ src/app/api/revalidate/route.js
@@ -4,18 +4,18 @@ import { NextResponse } from 'next/server';
 export const POST = async (req, res) => {
     const secret = '51528335b4a7e132-8a78cc1644bdd854-49429d9502d9f6a0';
 
-    const body = req.body;
-    const signature = req.headers['gcms-signature'];
-    const isValid = verifyWebhookSignature({ body, signature, secret });
+    // const body = req.body;
+    // const signature = req.headers['gcms-signature'];
+    // const isValid = verifyWebhookSignature({ body, signature, secret });
 
 
     if (req.method !== 'POST') {
         return res.status(405).json({ message: 'Method Not Allowed' });
     }
 
-    if (isValid) {
-        return new NextResponse(" Invalid token Error :(", { status: 401 });
-    }
+    // if (isValid) {
+    //     return new NextResponse(" Invalid token Error :(", { status: 401 });
+    // }
 
     try {
         // const data = req.body;
