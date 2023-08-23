5388dfdf3bb28b9a1702feada9295c714e28b3fc
Martynas-J <Aurimhas@gmail.com>
2023-08-23 22:57:54 +0300

    add vercel v4.1

--- src/app/api/revalidate/route.js
+++ src/app/api/revalidate/route.js
@@ -1,31 +1,28 @@
-import { generateWebhookSignature, verifyWebhookSignature } from '@hygraph/utils';
 import { NextResponse } from 'next/server';
+import { verifyWebhookSignature } from '@hygraph/utils';
 
-export const POST = async (req, res) => { 
-    const secret = '51528335b4a7e132-8a78cc1644bdd854-49429d9502d9f6a0';
+export const POST = async (req, res) => {
+    const secret = process.env.REVOKE_TOKEN;
 
-    const body = {
-        hello: "world",
-      };
-      
-      const signature = generateWebhookSignature({ body, secret });
-    
+    const body = {};
+    const signature = '...';
+    const isValid = verifyWebhookSignature({ body, signature, secret });
 
 
     if (req.method !== 'POST') {
         return res.status(405).json({ message: 'Method Not Allowed' });
     }
 
-    // if (isValid) {
-    //     return new NextResponse(" Invalid token Error :(", { status: 401 });
-    // }
+    if (!isValid) {
+        return new NextResponse(" Invalid token Error :(", { status: 401 });
+    }
 
     try {
         // const data = req.body;
         // console.log(data)
         // Perform any actions or data processing here
         // await res.revalidate("/");
-        return new NextResponse(signature, { status: 200 });
+        return new NextResponse("Veikia", { status: 200 });
     } catch (err) {
         return new NextResponse(" Error :(", { status: 500 });
     }
