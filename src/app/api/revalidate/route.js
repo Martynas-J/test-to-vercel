45eaab3fc64380f43f4f5b33f40c487ae14c4913
Martynas-J <Aurimhas@gmail.com>
2023-08-23 20:28:18 +0300

    add vercel v2.1

--- src/app/api/revalidate/route.js
+++ src/app/api/revalidate/route.js
@@ -1,27 +1,27 @@
 import { NextResponse } from "next/server";
 
 export const POST = async (req, res) => {
-  if (req.method !== 'POST') {
-    return res.status(405).json({ message: 'Method Not Allowed' });
-  }
+    if (req.method !== 'POST') {
+        return res.status(405).json({ message: 'Method Not Allowed' });
+    }
 
-  // Check for secret to confirm this is a valid request
-//   if (req.query.secret !== process.env.REVALIDATE_TOKEN) {
-//     return res.status(401).json({ message: 'Invalid token' });
-//   }
+    console.log(req)
+    // if (req.query.secret !== process.env.REVALIDATE_TOKEN) {
+    //     return new NextResponse(" Invalid token Error :(", { status: 401 });
+    // }
 
-  try {
-    // Process the data from the POST request
-    // You can access the request body using req.body
-    // For example:
-    // const data = req.body;
-    // console.log(data)
+    try {
+        // Process the data from the POST request
+        // You can access the request body using req.body
+        // For example:
+        // const data = req.body;
+        // console.log(data)
 
-    // Perform any actions or data processing here
+        // Perform any actions or data processing here
 
-    // await res.revalidate("/");
-    return new NextResponse("Veikia", { status: 200 });
-  } catch (err) {
-    return new NextResponse(" Error :(", { status: 500 });
-  }
+        // await res.revalidate("/");
+        return new NextResponse("Veikia", { status: 200 });
+    } catch (err) {
+        return new NextResponse(" Error :(", { status: 500 });
+    }
 }
