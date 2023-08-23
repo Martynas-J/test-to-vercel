0f816f1bc71848d4b99d158c9c7b4736bbaf56d0
Martynas-J <Aurimhas@gmail.com>
2023-08-23 23:40:15 +0300

    add vercel v5.3

--- src/app/api/revalidate/route.js
+++ src/app/api/revalidate/route.js
@@ -5,7 +5,7 @@ export const POST = async (req, res) => {
     const secret = "2e9291f10d44ca10204a4cd81b05d73b6a316b2b605d4e2e0e0b37b40198ce1f";
 
     const body = req.body;
-    const signature = "sign=rORNNBjFda8SaTKCyb4nSWo74hVqaMKE+qH36Dd0W1o=, env=master, t=1692823057842";
+    const signature = "sign=rORNNBjFda8SaTKCyb4nSWo74hVqaMKE+qH36Dd0W1oy, env=master, t=1692823057842";
     const isValid = verifyWebhookSignature({ body, signature, secret });
 
 
