e5a48d03604ed66f3bb79a3a39e6e0ed8af885f0
Martynas-J <Aurimhas@gmail.com>
2023-08-23 19:38:31 +0300

    add vercel v1.7

--- src/app/api/revalidate/route.js
+++ src/app/api/revalidate/route.js
@@ -1,26 +1,27 @@
+import { NextResponse } from "next/server";
 
-export default async function handler(req, res) {
+export const POST = async (req, res) => {
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Method Not Allowed' });
   }
 
   // Check for secret to confirm this is a valid request
-  if (req.query.secret !== process.env.REVALIDATE_TOKEN) {
-    return res.status(401).json({ message: 'Invalid token' });
-  }
+//   if (req.query.secret !== process.env.REVALIDATE_TOKEN) {
+//     return res.status(401).json({ message: 'Invalid token' });
+//   }
 
   try {
     // Process the data from the POST request
     // You can access the request body using req.body
     // For example:
-    const data = req.body;
-    console.log(data)
+    // const data = req.body;
+    // console.log(data)
 
     // Perform any actions or data processing here
 
-    await res.revalidate("/");
-    return res.status(200).json({ revalidated: true });
+    // await res.revalidate("/");
+    return new NextResponse("Veikia", { status: 200 });
   } catch (err) {
-    return res.status(500).send('Error processing webhook');
+    return new NextResponse(" Error :(", { status: 500 });
   }
 }
