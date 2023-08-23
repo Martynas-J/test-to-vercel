20a8c23beb814098ccbe82ba5f8d4de50f6e5b69
Martynas-J <Aurimhas@gmail.com>
2023-08-23 20:07:38 +0300

    add vercel v2.0

--- src/app/api/revalidate/route.js
+++ src/app/api/revalidate/route.js
@@ -6,9 +6,9 @@ export const POST = async (req, res) => {
   }
 
   // Check for secret to confirm this is a valid request
-  if (req.query.secret !== process.env.REVALIDATE_TOKEN) {
-    return res.status(401).json({ message: 'Invalid token' });
-  }
+//   if (req.query.secret !== process.env.REVALIDATE_TOKEN) {
+//     return res.status(401).json({ message: 'Invalid token' });
+//   }
 
   try {
     // Process the data from the POST request
