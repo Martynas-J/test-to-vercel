90646207fd7236e1728ecb8a9fe03e6160ee33b2
Martynas-J <Aurimhas@gmail.com>
2023-08-24 01:09:23 +0300

    add vercel v6.9

--- src/app/api/revalidate/route.js
+++ src/app/api/revalidate/route.js
@@ -8,7 +8,9 @@ export const POST = async (req, res) => {
     const signature = generateWebhookSignature({ body, secret });
     const isValid = verifyWebhookSignature({ body, signature, secret });
 
-    console.log(await req.headers["gcms-signature"])
+    const gcmsSignatureHeader = 'gcms-signature';
+    const gcmsSignatureValue = req.headers[gcmsSignatureHeader];
+    console.log(gcmsSignatureValue)
     if (req.method !== 'POST') {
         return res.status(405).json({ message: 'Method Not Allowed' });
     }
