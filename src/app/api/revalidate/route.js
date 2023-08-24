bbaca3217caf9ee9595297ce64c22ff6e1053dc5
Martynas-J <Aurimhas@gmail.com>
2023-08-24 09:27:05 +0300

    add vercel v8.7

--- src/app/api/revalidate/route.js
+++ src/app/api/revalidate/route.js
@@ -2,30 +2,22 @@ import { NextResponse } from 'next/server';
 import { generateWebhookSignature, verifyWebhookSignature } from '@hygraph/utils';
 import { headers } from "next/headers";
 import { revalidatePath } from 'next/cache'
+import { NextApiRequest, NextApiResponse } from 'next';
 
 
 export const POST = async (req, res) => {
     const secret = "2e9291f10d44ca10204a4cd81b05d73b6a316b2b605d4e2e0e0b37b40198ce1f";
-    const body = ""
+    const rawPayload = '{"hello":"world"}';
+
+
+
     const signature = headers().get("gcms-signature");
     console.log(signature)
-    console.log(generateWebhookSignature({ body, secret }))
-    console.log(generateWebhookSignature({ body, secret }))
-    console.log(generateWebhookSignature({ body, secret }))
-    console.log(generateWebhookSignature({ body, secret }))
-    console.log(generateWebhookSignature({ body, secret }))
-    console.log(generateWebhookSignature({ body, secret }))
-    console.log(generateWebhookSignature({ body, secret }))
+    console.log(generateWebhookSignature({ rawPayload, secret }))
 
-
-    const isValid = verifyWebhookSignature({ body, signature, secret });
-    const isValid2 = verifyWebhookSignature({ body, signature, secret });
-    const isValid3 = verifyWebhookSignature({ body, signature, secret });
-    const isValid4 = verifyWebhookSignature({ body, signature, secret });
+    const isValid = verifyWebhookSignature({ rawPayload, signature, secret });
     console.log(isValid)
-    console.log(isValid2)
-    console.log(isValid3)
-    console.log(isValid4)
+  
 
 
 
