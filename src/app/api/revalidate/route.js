975824471d3bb039e29bc825763045ce79e6aa45
Martynas-J <Aurimhas@gmail.com>
2023-08-24 15:24:27 +0300

    add vercel v12,5

--- src/app/api/revalidate/route.js
+++ src/app/api/revalidate/route.js
@@ -1,31 +1,16 @@
 import { NextResponse } from 'next/server';
 import { headers } from "next/headers";
 import { revalidatePath } from 'next/cache'
+const { verifyWebhookSignature } = require('@hygraph/utils');
 
 export const POST = async (req, res) => {
     const secret = "2e9291f10d44ca10204a4cd81b05d73b6a316b2b605d4e2e0e0b37b40198ce1f";
 
     const signature = headers().get("gcms-signature");
 
-    const [rawSign, rawEnv, rawTimestamp] = signature.split(', ');
-
-    const sign = rawSign.replace('sign=', '');
-    const EnvironmentName = rawEnv.replace('env=', '');
-    const Timestamp = parseInt(rawTimestamp.replace('t=', ''));
-
     const bodyReq = await req.json()
     const body = JSON.stringify(bodyReq)
-
-    let payload = JSON.stringify({
-        Body: body,
-        EnvironmentName,
-        TimeStamp: Timestamp,
-    });
-
-    const { createHmac } = require('crypto');
-
-    const hash = createHmac('sha256', secret).update(payload).digest('base64');
-    const isValid = sign === hash;
+    const isValid = verifyWebhookSignature({ body, signature, secret });
 
     if (req.method !== 'POST') {
         return res.status(405).json({ message: 'Method Not Allowed' });
