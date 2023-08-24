90e1dc850109c60dbcc8bbad9788df5851f729c6
Martynas-J <Aurimhas@gmail.com>
2023-08-24 08:13:41 +0300

    add vercel v7.8

--- src/app/api/revalidate/route.js
+++ src/app/api/revalidate/route.js
@@ -6,14 +6,8 @@ import { headers } from "next/headers";
 export const POST = async (req, res) => {
     const secret = "2e9291f10d44ca10204a4cd81b05d73b6a316b2b605d4e2e0e0b37b40198ce1f";
 
-    const headersList = headers();
-    const referer = headersList.get("gcms-signature");
-
-    console.log(referer)
-    const body = req.body;
-    console.log(generateWebhookSignature({ body, secret }))
-    const signature = referer;
-    const isValid = verifyWebhookSignature({ body, signature, secret });
+    const signature = headers().get("gcms-signature");
+    const isValid = verifyWebhookSignature({ signature, secret });
 
 
 
