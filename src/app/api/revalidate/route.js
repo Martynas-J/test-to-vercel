28274da2971c978f0b7108bff9a7d6741d7973f0
Martynas-J <Aurimhas@gmail.com>
2023-08-24 11:09:41 +0300

    add vercel v9.2

--- src/app/api/revalidate/route.js
+++ src/app/api/revalidate/route.js
@@ -1,15 +1,10 @@
 import { NextResponse } from 'next/server';
-import { generateWebhookSignature, verifyWebhookSignature } from '@hygraph/utils';
 import { headers } from "next/headers";
 import { revalidatePath } from 'next/cache'
 
-
 export const POST = async (req, res) => {
     const secret = "2e9291f10d44ca10204a4cd81b05d73b6a316b2b605d4e2e0e0b37b40198ce1f";
-    const rawPayload = '{"hello":"world"}';
 
-
-    console.log(req.headers)
     const signature = headers().get("gcms-signature");
 
     const [rawSign, rawEnv, rawTimestamp] = signature.split(', ');
@@ -29,9 +24,6 @@ export const POST = async (req, res) => {
     const hash = createHmac('sha256', secret).update(payload).digest('base64');
     const isValid = sign === hash;
     console.log(isValid)
-
-
-
 
     if (req.method !== 'POST') {
         return res.status(405).json({ message: 'Method Not Allowed' });
