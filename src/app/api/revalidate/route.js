7cc5cac941b9faea15e05b1edd771064966ab288
Martynas-J <Aurimhas@gmail.com>
2023-08-24 08:22:37 +0300

    add vercel v8.1

--- src/app/api/revalidate/route.js
+++ src/app/api/revalidate/route.js
@@ -6,46 +6,7 @@ import { revalidatePath } from 'next/cache'
 
 export const POST = async (req, res) => {
     const secret = "2e9291f10d44ca10204a4cd81b05d73b6a316b2b605d4e2e0e0b37b40198ce1f";
-    const body = {
-        "operation": "publish",
-        "data": {
-          "__typename": "Post",
-          "content": {
-            "__typename": "RichText",
-            "raw": {
-              "children": [
-                {
-                  "children": [
-                    {
-                      "text": "sdf"
-                    }
-                  ],
-                  "type": "paragraph"
-                }
-              ]
-            }
-          },
-          "createdAt": "2023-08-24T05:18:30.113252+00:00",
-          "createdBy": {
-            "__typename": "User",
-            "id": "clljx52d51ri901t4axmnfggi"
-          },
-          "id": "cllopqxgs4set0blbsn3xh640",
-          "publishedAt": "2023-08-24T05:18:30.783825+00:00",
-          "publishedBy": {
-            "__typename": "User",
-            "id": "clljx52d51ri901t4axmnfggi"
-          },
-          "scheduledIn": [],
-          "stage": "PUBLISHED",
-          "title": "sdf",
-          "updatedAt": "2023-08-24T05:18:30.113252+00:00",
-          "updatedBy": {
-            "__typename": "User",
-            "id": "clljx52d51ri901t4axmnfggi"
-          }
-        }
-      }
+    const body = {}
     const signature = headers().get("gcms-signature");
     const isValid = verifyWebhookSignature({ body, signature, secret });
 
@@ -55,9 +16,9 @@ export const POST = async (req, res) => {
         return res.status(405).json({ message: 'Method Not Allowed' });
     }
 
-    if (!isValid) {
-        return new NextResponse(" Invalid token Error :(", { status: 401 });
-    }
+    // if (!isValid) {
+    //     return new NextResponse(" Invalid token Error :(", { status: 401 });
+    // }
 
     try {
         // const data = req.body;
